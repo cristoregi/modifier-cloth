c274711bc520fc5bb961e10fb499a424c2ee228b
Daud <crhist16@gmail.com>
2019-11-28 11:11:59 +0700

    Initial set up for tailwindcss

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+	<div  class="h-full">
+		<nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+			<div class="flex items-center flex-shrink-0 text-white mr-6">
+				<img width="130px" src="@/assets/images/modify.png">
+			</div>
+			<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+				<div class="text-md lg:flex-grow">
+					<a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-orange-400 text-white mr-4">
+						Shop
+					</a>
+					<a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-orange-400 text-white mr-4">
+						About Us
+					</a>
+				</div>
+				<div>
+					<a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 hover:text-orange-400 text-white mr-4">
+						Cart
+					</a>
+				</div>
+				<div>
+					<span class="block mt-4 lg:inline-block lg:mt-0 hover:text-orange-400 text-white mr-4">
+						<i class="fas fa-user"></i> Profile
+					</span>
+				</div>
+			</div>
+		</nav>
+	</div>
+</template>
