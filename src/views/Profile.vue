2ceee6b84277f667dc24a885cf595610114a7217
Daud <crhist16@gmail.com>
2019-11-28 18:35:43 +0700

    WTF GREG

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,36 +1,34 @@
 <template>
-    <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
-    
-	<div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
-	
+  <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+		<div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
+			<div class="p-4 md:p-12 text-center lg:text-left">
+				<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://source.unsplash.com/MP0IUfwrn0A')"></div>
+				
+				<h1 class="text-3xl font-bold pt-8 lg:pt-0">Alisha Aileen</h1>
+				<div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>
+				<p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start "> <span class="mr-2"><i class="fas fa-graduation-cap"></i></span> College Students</p>
+				
+				<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
+					<span class="mr-4"><i class="fas fa-user"></i></span>
+					alishaleen</p>
+				<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
+					<span class="mr-4"><i class="fas fa-birthday-cake"></i></span>
+					21th July, 1999</p>
+				<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
+					<span class="mr-4"><i class="far fa-envelope-open"></i></span>
+					alishaaileen@gmail.com</p>
+				<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
+					<span class="mr-4"><i class="fas fa-phone"></i></span>
+					+62 827517263527
+				</p>
+			</div>
 
-		<div class="p-4 md:p-12 text-center lg:text-left">
-			<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://source.unsplash.com/MP0IUfwrn0A')"></div>
-			
-			<h1 class="text-3xl font-bold pt-8 lg:pt-0">Alisha Aileen</h1>
-			<div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-teal-500 opacity-25"></div>
-			<p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start "> <span class="mr-2"><i class="fas fa-graduation-cap"></i></span> College Students</p>
-			
-			<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-                <span class="mr-4"><i class="fas fa-user"></i></span>
-                alishaleen</p>
-			<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-                <span class="mr-4"><i class="fas fa-birthday-cake"></i></span>
-                21th July, 1999</p>
-			<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-                <span class="mr-4"><i class="far fa-envelope-open"></i></span>
-                alishaaileen@gmail.com</p>
-			<p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-                <span class="mr-4"><i class="fas fa-phone"></i></span>
-                +62 827517263527</p>
 		</div>
-
+		<div class="w-full lg:w-2/5">
+			<img src="https://source.unsplash.com/MP0IUfwrn0A" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
+		</div>
+		<!-- <div class="absolute top-0 right-0 h-12 w-18 p-4">
+			<button class="js-change-theme focus:outline-none">🌙</button>
+		</div> -->
 	</div>
-	<div class="w-full lg:w-2/5">
-		<img src="https://source.unsplash.com/MP0IUfwrn0A" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
-	</div>
-	  <div class="absolute top-0 right-0 h-12 w-18 p-4">
-		<button class="js-change-theme focus:outline-none">🌙</button>
-	  </div>
-</div>
 </template>
